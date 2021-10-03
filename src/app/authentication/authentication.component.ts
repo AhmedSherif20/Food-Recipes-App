1dde35721a137d128f0e9bef32370aeae6e10b49
ahmedsherif20 <aymanmard12@gmail.com>
2021-10-03 21:32:00 +0200

    auth component style done

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
   styleUrls: ['./authentication.component.scss']
 })
-export class AuthenticationComponent implements OnInit {
+export class AuthenticationComponent implements OnInit, AfterViewInit {
+
+  @ViewChild("goRegister") goRegisterBtn: ElementRef;
+  @ViewChild("goLogin") goLogin: ElementRef;
+  @ViewChild("formLayout") formLayout: ElementRef;
+
 
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  }
+  ngAfterViewInit(): void {
+    console.log(this.formLayout.nativeElement.classList);
+
+  }
 }
